8619827a9b96edd4200f2d4427e14589dbcb3f8f
arsalan.ahmed <arsalanabc@gmail.com>
2020-03-19 16:55:48 -0400

    Add end point for creating dummy listings

--- listings-service/src/server/routes.js
+++ listings-service/src/server/routes.js
@@ -5,6 +5,25 @@ const routes = app => {
         const listings = await Listing.findAll();
         return res.json(listings);
     });
+
+    app.get('/create-dummy-listings', async (req, res, next) => {
+        const title = "some Title";
+        const description = "some Description";
+
+        const dummyListings = [
+            { title: "title 1", description: "description 1" },
+            { title: "title 2", description: "description 2" },
+            { title: "title 3", description: "description 3" },
+            { title: "title 4", description: "description 4" },
+            { title: "title 5", description: "description 5" },
+        ]
+        Listing.bulkCreate(dummyListings)
+            .then((data) => {
+                data
+                ?res.send(data)
+                :res.status(400).send('Error in insert new record');
+            });
+    });
 };
 
 export default routes;
