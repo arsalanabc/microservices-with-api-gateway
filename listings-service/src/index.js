7a5025e524adfc7fb387ccaa986aa57778220701
arsalan.ahmed <arsalanabc@gmail.com>
2020-03-19 14:29:31 -0400

    Setup Express server and DB for listings service

--- listings-service/src/index.js
+++ listings-service/src/index.js
@@ -1,3 +1,6 @@
-import path from "path";
+import "@babel/polyfill";
+
+import "#root/db/connection";
+import "#root/server/startServer"; 
 
 console.log("this is working from listings service last ");
---
+++ listings-service/src/server/startServer.js
@@ -0,0 +1,22 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+
+import accessEnv from "#root/helpers/accessEnv";
+
+const PORT = accessEnv("PORT", 7100); // running listings service on 7100
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+    cors({
+            origin: (origin, cb) => cb(null, true),
+            credentials: true
+        })
+);
+
+app.listen(PORT, "0.0.0.0", ()=>{
+    console.info(`listings service is running on ${PORT}`);
+});
