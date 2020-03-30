9feceb710f5fd0fa69ac317de0d3376bcd8f778c
arsalan.ahmed <arsalanabc@gmail.com>
2020-03-30 19:23:09 -0400

    Make var names consistent

--- api-gateway/src/adaptors/ListingsService.js
+++ api-gateway/src/adaptors/ListingsService.js
@@ -1,10 +1,10 @@
 import got from "got";
 
-const LISTINGS_SERVICE = "http://listings-service:7100";
+const LISTINGS_SERVICE_API = "http://listings-service:7100";
 
 export default class ListingsService {
     static async fetchAllListings(){
-        const body = await got.get(`${LISTINGS_SERVICE}/listings`).json();
+        const body = await got.get(`${LISTINGS_SERVICE_API}/listings`).json();
         return body;
     }
 }
