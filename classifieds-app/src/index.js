bc65a8cbd3bcfa215b1b78c3b8446e83e37ca66f
arsalan.ahmed <arsalanabc@gmail.com>
2020-04-04 13:55:32 -0400

    Add global styling to app component

--- classifieds-app/src/index.js
+++ classifieds-app/src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import { render } from "react-dom";
+import { createGlobalStyle } from "styled-components";
 
 import App from "./app";
 
-render(<App />, document.getElementById("app"));
+
+const GlobalSyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+html, body, #app{
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+}
+body {
+    font-family: 'Roboto', sans-serif;
+}
+`;
+
+render(<>
+    <GlobalSyle />
+    <App />
+    </>, document.getElementById("app"));
