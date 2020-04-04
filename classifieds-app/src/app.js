a254d3c9523a33e6aaeaa6e7db0e862334b4fa0d
arsalan.ahmed <arsalanabc@gmail.com>
2020-04-04 13:41:23 -0400

    Create App.js

---
+++ classifieds-app/src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App =() => {
+    return(<h1>Working from app js</h1>);
+};
+
+export default App;
+
+
--- classifieds-app/src/index.js
+++ classifieds-app/src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-render(<h1> app working</h1>, document.getElementById("app"));
+import App from "./app";
+
+render(<App />, document.getElementById("app"));
